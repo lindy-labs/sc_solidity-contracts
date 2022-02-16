7e505a563d768daf344b07885feada34d0031f8b
Gabriel Poca <mail@gabrielpoca.com>
2022-02-16 15:46:14 +0000

    wip

--- subgraph/tests/integreation.test.ts
+++ subgraph/tests/integreation.test.ts
@@ -1,17 +1,5 @@
-import {
-  log,
-  ethereum,
-  Address,
-  BigInt,
-  Bytes,
-  ByteArray
-} from "@graphprotocol/graph-ts";
-import {
-  clearStore,
-  test,
-  assert,
-  newMockEvent
-} from "matchstick-as/assembly/index";
+import { log, ethereum, Address, Bytes } from "@graphprotocol/graph-ts";
+import { test, assert, newMockEvent } from "matchstick-as/assembly/index";
 
 import {
   handleDepositMinted,
@@ -24,6 +12,8 @@ import {
 } from "../src/types/templates/Vault/IVaultSponsoring";
 import { DepositMinted } from "../src/types/templates/Vault/IVault";
 import { Vault } from "../generated/schema";
+
+const MOCK_ADDRESS_1 = "0xC80B3caAd6d2DE80Ac76a41d5F0072E36D2519Cd".toLowerCase();
 
 test("handleSponsored create a sponsor", () => {
   let mockEvent = newMockEvent();
@@ -38,25 +28,11 @@ test("handleSponsored create a sponsor", () => {
   );
   event.parameters = new Array();
 
-  const idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(1000));
-  const amount = new ethereum.EventParam(
-    "amount",
-    ethereum.Value.fromI32(1000)
-  );
-  const depositor = new ethereum.EventParam(
-    "depositor",
-    ethereum.Value.fromAddress(
-      Address.fromString("0xC80B3caAd6d2DE80Ac76a41d5F0072E36D2519Cd")
-    )
-  );
-  const lockedUntil = new ethereum.EventParam(
-    "lockedUntil",
-    ethereum.Value.fromI32(1000)
-  );
-  const burned = new ethereum.EventParam(
-    "burned",
-    ethereum.Value.fromBoolean(false)
-  );
+  const idParam = newI32("id", 1000);
+  const amount = newI32("amount", 1000);
+  const depositor = newAddress("depositor", MOCK_ADDRESS_1);
+  const lockedUntil = newI32("lockedUntil", 1000);
+  const burned = newBool("burned", false);
 
   event.parameters.push(idParam);
   event.parameters.push(amount);
@@ -67,14 +43,7 @@ test("handleSponsored create a sponsor", () => {
   handleSponsored(event);
 
   assert.fieldEquals("Sponsor", "1000", "amount", "1000");
-
-  assert.fieldEquals(
-    "Sponsor",
-    "1000",
-    "depositor",
-    "0xc80b3caad6d2de80ac76a41d5f0072e36d2519cd"
-  );
-
+  assert.fieldEquals("Sponsor", "1000", "depositor", MOCK_ADDRESS_1);
   assert.fieldEquals("Sponsor", "1000", "burned", "false");
 });
 
@@ -91,7 +60,7 @@ test("handleUnsponsored removes a sponsor", () => {
   );
   event.parameters = new Array();
 
-  const idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(1000));
+  const idParam = newI32("id", 1000);
 
   event.parameters.push(idParam);
 
@@ -116,33 +85,15 @@ test("handleDepositMinted creates a deposit", () => {
   const vault = new Vault(mockEvent.address.toString());
   vault.save();
 
-  const idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(1000));
-  const groupId = new ethereum.EventParam(
-    "groupId",
-    ethereum.Value.fromI32(1000)
-  );
-  const amount = new ethereum.EventParam(
-    "amount",
-    ethereum.Value.fromI32(1000)
-  );
+  const idParam = newI32("id", 1000);
+  const groupId = newI32("groupId", 1000);
+  const amount = newI32("amount", 1000);
   const shares = newI32("shares", 1000);
-  const depositor = newAddress(
-    "depositor",
-    "0xC80B3caAd6d2DE80Ac76a41d5F0072E36D2519Cd"
-  );
-  const claimer = newAddress(
-    "claimer",
-    "0xC80B3caAd6d2DE80Ac76a41d5F0072E36D2519Cd"
-  );
+  const depositor = newAddress("depositor", MOCK_ADDRESS_1);
+  const claimer = newAddress("claimer", MOCK_ADDRESS_1);
   const claimerId = newI32("claimerId", 1000);
-  const lockedUntil = new ethereum.EventParam(
-    "lockedUntil",
-    ethereum.Value.fromI32(1000)
-  );
-  const data = new ethereum.EventParam(
-    "data",
-    ethereum.Value.fromBytes(Bytes.empty())
-  );
+  const lockedUntil = newI32("lockedUntil", 1000);
+  const data = newBytes("data", Bytes.empty());
 
   event.parameters.push(idParam);
   event.parameters.push(groupId);
@@ -157,20 +108,21 @@ test("handleDepositMinted creates a deposit", () => {
   handleDepositMinted(event);
 
   assert.fieldEquals("Deposit", "1000", "amount", "1000");
-
-  assert.fieldEquals(
-    "Deposit",
-    "1000",
-    "depositor",
-    "0xc80b3caad6d2de80ac76a41d5f0072e36d2519cd"
-  );
-
+  assert.fieldEquals("Deposit", "1000", "depositor", MOCK_ADDRESS_1);
   assert.fieldEquals("Claimer", "1000", "principal", "1000");
   assert.fieldEquals("Claimer", "1000", "depositsIds", "[1000]");
 });
 
+function newBytes(name: string, value: Bytes): ethereum.EventParam {
+  return new ethereum.EventParam(name, ethereum.Value.fromBytes(value));
+}
+
 function newI32(name: string, value: i32): ethereum.EventParam {
   return new ethereum.EventParam(name, ethereum.Value.fromI32(value));
+}
+
+function newBool(name: string, value: bool): ethereum.EventParam {
+  return new ethereum.EventParam(name, ethereum.Value.fromBoolean(value));
 }
 
 function newAddress(name: string, value: string): ethereum.EventParam {
