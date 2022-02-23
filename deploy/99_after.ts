a89deaf8c6e709351fb443ef0a1fc856836bae16
Miguel Palhas <mpalhas@gmail.com>
2022-02-23 18:23:27 +0000

    Remove the SandclockFactory (#30)

--- deploy/99_after.ts
+++
@@ -1,9 +0,0 @@
-import type { DeployFunction } from "hardhat-deploy/types";
-
-import { printLog } from "../scripts/deployHelpers";
-
-const func: DeployFunction = async function (env) {
-  printLog();
-};
-
-export default func;
