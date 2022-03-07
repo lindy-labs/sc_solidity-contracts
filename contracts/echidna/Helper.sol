80ba73fe2f4ffbbbb2805a16486785926302aff1
coolhill <8256597+coolhill@users.noreply.github.com>
2022-03-07 12:42:52 +0100

    help function for valid inputs

--- contracts/echidna/Helper.sol
+++ contracts/echidna/Helper.sol
@@ -5,6 +5,10 @@ import "./Addresses.sol";
 contract Helper is Addresses {
     function mint_helper(address recip, uint256 amount) internal {
         underlying.mint(recip, amount);
-	underlying.approve(address(vault), amount);
+        underlying.approve(address(vault), amount);
+    }
+
+    function one_to_max_uint64(uint256 random) internal returns (uint256) {
+        return 1 + (random % (type(uint64).max - 1));
     }
 }
