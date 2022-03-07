12a116085476c77f87fba45cce8569abf64f56cf
coolhill <8256597+coolhill@users.noreply.github.com>
2022-03-07 15:34:20 +0100

    double deployment bug fix

--- contracts/echidna/Addresses.sol
+++ contracts/echidna/Addresses.sol
@@ -6,5 +6,5 @@ import "../mock/MockERC20.sol";
 
 contract Addresses {
     Vault vault = Vault(0x48BaCB9266a570d521063EF5dD96e61686DbE788);
-    TestERC20 underlying = TestERC20(0x1dC4c1cEFEF38a777b15aA20260a54E584b16C48);
+    MockERC20 underlying = MockERC20(0x1dC4c1cEFEF38a777b15aA20260a54E584b16C48);
 }
