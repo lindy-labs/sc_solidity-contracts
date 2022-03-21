f84266873e3e1fa2cf2acc7bd3de7d85d5766bd7
0xrin1 <0xrin1@protonmail.com>
2022-03-21 19:06:42 +0000

    [WIP] Codegen before subgraph compilation and remove StrategyUpdated event and handler function

--- subgraph/src/mappings/strategy.ts
+++ subgraph/src/mappings/strategy.ts
@@ -1,26 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
-// import { NewVault } from "../types/SandclockFactory/SandclockFactory";
-// import { Vault as VaultContract } from "../types/SandclockFactory/Vault";
 import { Strategy as StrategyContract } from "../types/Vault/Strategy";
-
-// import { Vault as VaultTemplate } from "../types/templates";
-import { Strategy as StrategyTemplate } from "../types/templates";
-// import { Claimers as ClaimersTemplate } from "../types/templates";
-
-// import { Vault } from "../types/schema";
-// import { Strategy } from "../types/schema";
-import { StrategyUpdated } from "../types/Vault/Vault";
-
-export function handleStrategyUpdated(event: StrategyUpdated): void {
-  let contract = StrategyContract.bind(event.params.strategy);
-
-  // let record = new Strategy(event.params.strategy.toHexString());
-
-  StrategyTemplate.create(event.params.strategy);
-  // ClaimersTemplate.create(contract.claimers());
-  // record.save();
-}
 
 export function handleInitDeposit(event: Transfer): void {
   // const claimerId = event.params.tokenId.toHexString();
