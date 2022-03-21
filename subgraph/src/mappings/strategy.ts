0a891b271d49fa9cc3836afd41345d39717e79a9
0xrin1 <0xrin1@protonmail.com>
2022-03-21 20:07:14 +0000

    [WIP] Implement handler logic

--- subgraph/src/mappings/strategy.ts
+++ subgraph/src/mappings/strategy.ts
@@ -1,17 +1,28 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { InitDepositOperation, InitRedeemOperation } from "../types/schema";
 
-import { Strategy as StrategyContract } from "../types/Vault/Strategy";
+import {
+  InitDepositStable,
+  InitRedeemStable,
+} from "../types/Strategy/AnchorUSTStrategy";
 
-export function handleInitDeposit(event: Transfer): void {
-  // const claimerId = event.params.tokenId.toHexString();
-  // const claimer = Claimer.load(claimerId)!;
-  // claimer.owner = event.params.to;
-  // claimer.save();
+export function handleInitDeposit(event: InitDepositStable): void {
+  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+
+  const initDepositOperation = new InitDepositOperation(id);
+  initDepositOperation.idx = event.params.idx;
+  initDepositOperation.operator = event.params.operator;
+  initDepositOperation.underlyingAmount = event.params.underlyingAmount;
+  initDepositOperation.ustAmount = event.params.ustAmount;
+
+  initDepositOperation.save();
 }
 
-export function handleInitRedeem(event: Transfer): void {
-  // const claimerId = event.params.tokenId.toHexString();
-  // const claimer = Claimer.load(claimerId)!;
-  // claimer.owner = event.params.to;
-  // claimer.save();
+export function handleInitRedeem(event: InitRedeemStable): void {
+  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+
+  const initRedeemOperation = new InitRedeemOperation(id);
+  initRedeemOperation.operator = event.params.operator;
+  initRedeemOperation.aUstAmount = event.params.aUstAmount;
+
+  initRedeemOperation.save();
 }
