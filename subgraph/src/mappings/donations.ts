75a720b494e97f55cff55d44bb11a17cf556ce6d
0xrin1 <0xrin1@protonmail.com>
2022-05-12 16:48:39 +0200

    [WIP] Donations graph dataSource

---
+++ subgraph/src/mappings/donations.ts
@@ -0,0 +1,9 @@
+import { store } from '@graphprotocol/graph-ts';
+import { Donation } from '../types/schema';
+import {
+  DonationMinted,
+} from '../types/Donations';
+
+export function handleDonationMinted(event: DonationMinted): void {
+
+}
