3697efda8eb7e12620c5aa1ed428c202789a8bf9
0xrin1 <0xrin1@protonmail.com>
2022-05-16 13:04:45 +0200

    [WIP] Introduce donation minted event

--- subgraph/src/mappings/donations.ts
+++ subgraph/src/mappings/donations.ts
@@ -2,7 +2,7 @@ import { store } from '@graphprotocol/graph-ts';
 import { Donation } from '../types/schema';
 import {
   DonationMinted,
-} from '../types/Donations';
+} from '../types/Donations/Donations';
 
 export function handleDonationMinted(event: DonationMinted): void {
 
