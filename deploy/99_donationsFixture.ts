828e3b4eef2183a11aa1cac54da9bee318cfba00
0xrin1 <0xrin1@protonmail.com>
2022-05-25 11:10:26 +0200

    adjust donations fixture to mint as one batch

--- deploy/99_donationsFixture.ts
+++ deploy/99_donationsFixture.ts
@@ -32,27 +32,23 @@ const func = async function (env: HardhatRuntimeEnvironment) {
       // subgraph handler for YieldClaimed event
       donationId:
         '0xc21191fcea1d9acbfbe513a5ca5993c82500a4be541f419d191c1017ed374d66-1-0',
-    },
-  ]);
-
-  ({ transactionHash, args } = yieldClaimedEvents[1]);
-
-  await donations.mint(transactionHash, 1, [
-    {
+    }, {
       destinationId: 10,
       owner: args.claimerId,
       token: UST.address,
       amount: args.amount,
       donationId:
-        '0xcb6eec5feb0518ea854521c5b91baf5ababba545ddfc7e87162a9b95169dcf57-1-0',
+        '0xc21191fcea1d9acbfbe513a5ca5993c82500a4be541f419d191c1017ed374d66-1-1',
     },
   ]);
+
+  ({ transactionHash, args } = yieldClaimedEvents[1]);
 
   // Move time forward more than 180 days
   await ethers.provider.send("evm_increaseTime", [1.6e7]);
   await ethers.provider.send("evm_mine", []);
 
-  await donations.burn(1, '0xcb6eec5feb0518ea854521c5b91baf5ababba545ddfc7e87162a9b95169dcf57-1-0');
+  await donations.burn(1, '0xc21191fcea1d9acbfbe513a5ca5993c82500a4be541f419d191c1017ed374d66-1-1');
 };
 
 func.id = 'donations_fixture';
