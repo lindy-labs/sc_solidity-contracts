c7824506e4cff15dee1be521097f97356364b765
realdiganta <47485188+dxganta@users.noreply.github.com>
2022-06-01 15:51:47 +0530

    tenderly import removed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,22 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from 'hardhat/config';
 
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-web3";
-import "@nomiclabs/hardhat-etherscan";
-import "@primitivefi/hardhat-dodoc";
-import "@typechain/hardhat";
-import "hardhat-deploy";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@tenderly/hardhat-tenderly";
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-web3';
+import '@nomiclabs/hardhat-etherscan';
+import '@primitivefi/hardhat-dodoc';
+import '@typechain/hardhat';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
 
 const devMnemonic =
   process.env.MNEMONIC ||
-  "core tornado motion pigeon kiss dish differ asthma much ritual black foil";
+  'core tornado motion pigeon kiss dish differ asthma much ritual black foil';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.10",
+    version: '0.8.10',
     settings: {
       optimizer: {
         enabled: true,
@@ -29,13 +28,13 @@ const config: HardhatUserConfig = {
     hardhat: {
       accounts: {
         mnemonic: devMnemonic,
-        accountsBalance: "100000000000000000000000000",
+        accountsBalance: '100000000000000000000000000',
       },
       initialBaseFeePerGas: 0,
       chainId: 31337,
     },
     docker: {
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
       accounts: {
         mnemonic: devMnemonic,
       },
@@ -44,26 +43,26 @@ const config: HardhatUserConfig = {
       live: false,
     },
     etheno: {
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
       chainId: 1337,
       live: false,
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${
-        process.env.INFURA_KEY || "missing-key"
+        process.env.INFURA_KEY || 'missing-key'
       }`,
       chainId: 1,
       accounts: {
-        mnemonic: process.env.MAINNET_MNEMONIC || "TODO",
+        mnemonic: process.env.MAINNET_MNEMONIC || 'TODO',
       },
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${
-        process.env.INFURA_KEY || "missing-key"
+        process.env.INFURA_KEY || 'missing-key'
       }`,
       chainId: 3,
       accounts: {
-        mnemonic: process.env.TESTNET_MNEMONIC || "TODO",
+        mnemonic: process.env.TESTNET_MNEMONIC || 'TODO',
       },
     },
   },
@@ -79,22 +78,22 @@ const config: HardhatUserConfig = {
     timeout: 2000000,
   },
   typechain: {
-    outDir: "typechain",
+    outDir: 'typechain',
   },
   dodoc: {
     runOnCompile: !!process.env.COMPILE_DOCS || false,
     include: [
-      "Vault",
-      "Claimers",
-      "Depositors",
-      "IVault",
-      "IIntegration",
-      "PercentMath",
+      'Vault',
+      'Claimers',
+      'Depositors',
+      'IVault',
+      'IIntegration',
+      'PercentMath',
     ],
   },
   etherscan: {
     apiKey: {
-      ropsten: process.env.ETHERSCAN_KEY || "missing-key",
+      ropsten: process.env.ETHERSCAN_KEY || 'missing-key',
     },
   },
 };
