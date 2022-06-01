6d14044e9f79c127af7d3c6ca3bebd8108ed3cfa
0xrin1 <0xrin1@protonmail.com>
2022-06-01 19:09:16 +0200

    make deployConfigs types a bit stricter

--- scripts/deployConfigs.ts
+++ scripts/deployConfigs.ts
@@ -1,15 +1,21 @@
-import { network, getNamedAccounts } from "hardhat";
-import { Address } from "hardhat-deploy/types";
+import { network, getNamedAccounts } from 'hardhat';
+import { Address } from 'hardhat-deploy/types';
 
-interface Config {
-  investPct?: number;
-  perfFeePct?: number;
-  lossTolerancePct?: number;
+interface VaultConfig {
+  investPct: number;
+  perfFeePct: number;
+  lossTolerancePct: number;
   multisig: string;
-  minLockPeriod?: number;
-  ethAnchorRouter?: string;
-  AUstToUstPriceFeed?: string;
+  minLockPeriod: number;
+  ethAnchorRouter: string;
+  AUstToUstPriceFeed: string;
 }
+
+interface DonationsConfig {
+  multisig: string;
+}
+
+type Config = VaultConfig | DonationsConfig;
 
 const networkConfigs: Record<number, Config> = {
   // mainnet
@@ -17,10 +23,10 @@ const networkConfigs: Record<number, Config> = {
     investPct: 9000, // 90%
     perfFeePct: 300, // TODO
     lossTolerancePct: 200, // TODO
-    multisig: "0x035F210e5d14054E8AE5A6CFA76d643aA200D56E",
+    multisig: '0x035F210e5d14054E8AE5A6CFA76d643aA200D56E',
     minLockPeriod: 60 * 60 * 24 * 30, // 30 days
-    ethAnchorRouter: "0xcEF9E167d3f8806771e9bac1d4a0d568c39a9388",
-    AUstToUstPriceFeed: "0x7b80a92f7d1e5cEeDDf939d77BF281E7e88f2906",
+    ethAnchorRouter: '0xcEF9E167d3f8806771e9bac1d4a0d568c39a9388',
+    AUstToUstPriceFeed: '0x7b80a92f7d1e5cEeDDf939d77BF281E7e88f2906',
   },
 
   // ropsten
@@ -28,10 +34,10 @@ const networkConfigs: Record<number, Config> = {
     investPct: 9000, // 90%
     perfFeePct: 100, // 1%
     lossTolerancePct: 200,
-    multisig: "deployer",
+    multisig: 'deployer',
     minLockPeriod: 1, // 1 second
-    ethAnchorRouter: "0x7537aC093cE1315BCE08bBF0bf6f9b86B7475008",
-    AUstToUstPriceFeed: "TODO",
+    ethAnchorRouter: '0x7537aC093cE1315BCE08bBF0bf6f9b86B7475008',
+    AUstToUstPriceFeed: 'TODO',
   },
 
   // docker network
@@ -39,10 +45,10 @@ const networkConfigs: Record<number, Config> = {
     investPct: 9000, // 90%
     perfFeePct: 100, // 1%
     lossTolerancePct: 200,
-    multisig: "deployer",
+    multisig: 'deployer',
     minLockPeriod: 1, // 1 second
-    ethAnchorRouter: "TODO",
-    AUstToUstPriceFeed: "TODO",
+    ethAnchorRouter: 'TODO',
+    AUstToUstPriceFeed: 'TODO',
   },
 
   // hardhat
@@ -50,20 +56,20 @@ const networkConfigs: Record<number, Config> = {
     investPct: 9000, // 90%
     perfFeePct: 100, // 1%
     lossTolerancePct: 200,
-    multisig: "deployer",
+    multisig: 'deployer',
     minLockPeriod: 1, // 1 second
-    ethAnchorRouter: "TODO",
-    AUstToUstPriceFeed: "TODO",
+    ethAnchorRouter: 'TODO',
+    AUstToUstPriceFeed: 'TODO',
   },
 
   // polygon mumbai
   80001: {
-    multisig: "deployer",
+    multisig: 'deployer',
   },
-  
+
   // polygon mainnet
   137: {
-    multisig: "deployer",
+    multisig: 'deployer',
   },
 };
 
