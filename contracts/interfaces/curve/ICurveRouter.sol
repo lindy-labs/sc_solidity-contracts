401af2ef87ef91ea2a547fbbe0422eac3bcb1eff
realdiganta <47485188+dxganta@users.noreply.github.com>
2022-07-06 11:50:04 +0530

    Optimal Swap contract added

---
+++ contracts/interfaces/curve/ICurveRouter.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.10;
+
+interface ICurveRouter {
+    function get_best_rate(
+        address from,
+        address to,
+        uint256 _amount
+    ) external view returns (address, uint256);
+
+    function exchange_with_best_rate(
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint256 _expected
+    ) external returns (uint256);
+
+    function get_exchange_amount(
+        address _pool,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external view returns (uint256);
+
+    function exchange(
+        address _pool,
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint256 _expected,
+        address _receiver
+    ) external payable returns (uint256);
+}
