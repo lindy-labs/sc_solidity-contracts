b3c0a74b1827ebe51a6c507705031916c68b1cc7
Brkomir <nenad.milenkovic154@outlook.com>
2022-07-25 11:44:07 +0200

    removing import Ownable

--- contracts/strategy/yearn/YearnStrategy.sol
+++ contracts/strategy/yearn/YearnStrategy.sol
@@ -3,7 +3,6 @@ pragma solidity =0.8.10;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 import {PercentMath} from "../../lib/PercentMath.sol";
@@ -104,10 +103,6 @@ contract YearnStrategy is IStrategy, AccessControl, CustomErrors {
             revert StrategyCallerNotAdmin();
         _;
     }
-
-    //
-    // Ownable
-    //
 
     /**
      * Transfers administrator rights for the Strategy to another account,
