c7625aeed896e58a6bb851da61a87aaaab32f1d3
realdiganta <47485188+dxganta@users.noreply.github.com>
2022-07-27 12:36:03 +0530

    test passing

--- test/0x/0xapi.fork.spec.ts
+++ test/0x/0xapi.fork.spec.ts
@@ -1,29 +1,18 @@
 import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { BigNumber } from 'ethers';
-import { ethers, web3 } from 'hardhat';
-import { expect } from 'chai';
-import { time } from '@openzeppelin/test-helpers';
+import { ethers } from 'hardhat';
 
-import qs from 'qs';
 import axios from 'axios';
 
 import { SimpleTokenSwap, ERC20, ERC20__factory } from '../../typechain';
 
-import {
-  ForkHelpers,
-  generateNewAddress,
-  moveForwardTwoWeeks,
-} from '../shared';
+import { ForkHelpers } from '../shared';
+import { assert } from 'console';
 
 const { parseUnits } = ethers.utils;
-const { MaxUint256 } = ethers.constants;
-
-const FORK_BLOCK = 14988444;
 
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const LQTY = '0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D';
-const DAI = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 
 describe('0xapi swap mainnet fork tests', () => {
   let owner: SignerWithAddress;
@@ -31,16 +20,16 @@ describe('0xapi swap mainnet fork tests', () => {
   let simpleTokenSwap: SimpleTokenSwap;
   let lqty: ERC20;
   let weth: ERC20;
-  let dai: ERC20;
+  let usdc: ERC20;
 
   beforeEach(async () => {
-    await ForkHelpers.forkToMainnet(FORK_BLOCK);
+    await ForkHelpers.forkToMainnet();
 
     [owner, alice] = await ethers.getSigners();
 
     lqty = ERC20__factory.connect(LQTY, owner);
     weth = ERC20__factory.connect(WETH, owner);
-    dai = ERC20__factory.connect(DAI, owner);
+    usdc = ERC20__factory.connect(USDC, owner);
 
     const SimpleTokenSwapFactory = await ethers.getContractFactory(
       'SimpleTokenSwap',
@@ -62,8 +51,12 @@ describe('0xapi swap mainnet fork tests', () => {
 
     console.log(data);
 
+    assert((await usdc.balanceOf(simpleTokenSwap.address)).toNumber() == 0);
+
     await simpleTokenSwap
       .connect(owner)
       .fillQuote(data.sellTokenAddress, data.to, data.data);
+
+    assert((await usdc.balanceOf(simpleTokenSwap.address)).toNumber() > 0);
   });
 });
