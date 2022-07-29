fc99fee0b09ed1cefb3fdbc4419e48667ed8cb1b
0xrin1 <0xrin1@protonmail.com>
2022-07-29 19:48:17 +0200

    express slippage percentage in multiple of 100

--- test/shared/contracts.ts
+++ test/shared/contracts.ts
@@ -3,4 +3,4 @@ import { ethers } from 'hardhat';
 const BigNumber = ethers.BigNumber;
 
 export const SHARES_MULTIPLIER = BigNumber.from('10').pow(18);
-export const CURVE_SLIPPAGE = BigNumber.from('5');
+export const CURVE_SLIPPAGE = BigNumber.from('500');
