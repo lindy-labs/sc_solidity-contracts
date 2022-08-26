0e080aa0a3c3cbdcd8f84f4eaaa74a509a71d39e
0xrin1 <0xrin1@protonmail.com>
2022-08-26 16:29:45 +0200

    liquity strategy deployments

---
+++ deploy/liquityStrategy.ts
@@ -0,0 +1,105 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { includes } from 'lodash';
+import { ethers } from 'hardhat';
+import { utils } from 'ethers';
+import { getCurrentNetworkConfig } from '../scripts/deployConfigs';
+
+const func = async function (env: HardhatRuntimeEnvironment) {
+  const { deployer } = await env.getNamedAccounts();
+  const { get, deploy, getNetworkName } = env.deployments;
+  const { multisig } = await getCurrentNetworkConfig();
+  const [owner] = await ethers.getSigners();
+
+  const vaultAddress = (await get('Vault_Liquity')).address;
+  const vault = await ethers.getContractAt('Vault', vaultAddress);
+
+  const LUSDDeployment = await get('LUSD');
+
+  // if (getNetworkName() !== 'mainnet') {
+  //   await deploy('YearnVault', {
+  //     contract: 'MockYearnVault',
+  //     from: deployer,
+  //     args: ['LUSD Yearn Vault', 'LUSD', LUSDDeployment.address],
+  //   });
+  // }
+
+  // const yearnVault = await get('YearnVault');
+
+  // const args = [
+  //   vault.address,
+  //   owner.address,
+  //   yearnVault.address,
+  //   LUSDDeployment.address,
+  // ];
+
+  const liquityStrategyDeployment = await deploy('LiquityStrategy', {
+    contract: 'LiquityStrategy',
+    from: deployer,
+    args: [],
+    log: true,
+  });
+
+  const liquityStrategy = await ethers.getContractAt(
+    'LiquityStrategy',
+    liquityStrategyDeployment.address,
+  );
+
+  if (getNetworkName() !== 'hardhat' && getNetworkName() !== 'docker') {
+    try {
+      await env.run('verify:verify', {
+        address: liquityStrategy.address,
+        constructorArguments: [],
+      });
+    } catch (e) {
+      console.error((e as Error).message);
+    }
+
+    await env.tenderly.persistArtifacts({
+      name: 'LiquityStrategy',
+      address: liquityStrategy.address,
+    });
+  }
+
+  // initialize strategy
+  await (await liquityStrategy.initialize(
+    vault.address,
+    multisig,
+    '0x66017d22b0f8556afdd19fc67041899eb65a21bb',
+    '0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D', // TODO: dynamically set LQTY address
+    LUSDDeployment.address,
+  )).wait();
+
+  console.log('LquityStrategy initialized');
+
+  // set manager role
+  await liquityStrategy
+    .connect(owner)
+    .grantRole(
+      utils.keccak256(utils.toUtf8Bytes('MANAGER_ROLE')),
+      owner.address,
+    );
+
+  console.log('manager_role granted to owner for strategy');
+
+  await (
+    await vault.connect(owner).setStrategy(liquityStrategy.address)
+  ).wait();
+  console.log('strategy set to vault');
+
+  if (owner.address !== multisig) {
+    await (await vault.connect(owner).transferOwnership(multisig)).wait();
+    console.log('ownership transfered to multisig');
+  }
+};
+
+func.tags = ['strategy', 'custom_liquity'];
+func.dependencies = ['vault'];
+
+func.skip = async (env: HardhatRuntimeEnvironment) =>
+  !includes(
+    ['ropsten', 'docker', 'mainnet', 'hardhat'],
+    env.deployments.getNetworkName(),
+  );
+
+export default func;
