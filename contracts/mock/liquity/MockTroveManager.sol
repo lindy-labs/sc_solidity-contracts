e7691057c413a1f9c694ec87e8396ad0a762714b
0xrin <0xrin1@protonmail.com>
2022-09-07 14:49:35 +0200

    Liquity fixture to simulate TroveManager Liquidation (#192)

---
+++ contracts/mock/liquity/MockTroveManager.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.10;
+
+/* 
+ * Mock contract does not coincide with interface of production contract.
+ * Used to emit event as found in production contract to simply simulate events that
+ * rarely happen, for development.
+ */
+contract MockTroveManager {
+    string public constant NAME = "TroveManager";
+
+    event Liquidation(
+        uint256 _liquidatedDebt,
+        uint256 _liquidatedColl,
+        uint256 _collGasCompensation,
+        uint256 _LUSDGasCompensation
+    );
+
+    function liquidation(
+        uint256 _liquidatedDebt,
+        uint256 _liquidatedColl,
+        uint256 _collGasCompensation,
+        uint256 _LUSDGasCompensation
+    ) public {
+        emit Liquidation(
+            _liquidatedDebt,
+            _liquidatedColl,
+            _collGasCompensation,
+            _LUSDGasCompensation
+        );
+    }
+}
