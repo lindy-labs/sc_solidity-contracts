f97e2d50994813fd8e005c9dcadc5a8420bfd730
0xrin1 <0xrin1@protonmail.com>
2022-09-07 15:23:49 +0200

    prettier on liquity mapping

--- subgraph/ethereum/src/mappings/liquity.ts
+++ subgraph/ethereum/src/mappings/liquity.ts
@@ -1,20 +1,10 @@
 import { BigInt, log } from '@graphprotocol/graph-ts';
-// import {
-//   DepositWithdrawn,
-//   DepositMinted,
-//   YieldClaimed,
-// } from '../types/Vault/IVault';
-// import { TreasuryUpdated } from '../types/Vault/IVaultSettings';
-// import { Sponsored, Unsponsored } from '../types/Vault/IVaultSponsoring';
-import {
-  Liquidation,
-} from '../types/schema';
+import { Liquidation } from '../types/schema';
 
 export function handleLiquidation(event: Liquidation): void {
   const liquidation = new Liquidation(
-        event.transaction.hash.toHex() +
-        '-' +
-        event.logIndex.toString());
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString(),
+  );
 
   liquidation.txHash = event.transaction.hash.toHex();
   liquidation.liquidatedDebt = event.params.liquidatedDebt;
