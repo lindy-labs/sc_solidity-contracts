189ee97820c53eee84955658992c3e7cfa2d8ac9
0xrin1 <0xrin1@protonmail.com>
2022-09-15 20:28:21 +0200

    change liquidationId to transaction hash + logIndex

--- subgraph/ethereum/src/mappings/liquity.ts
+++ subgraph/ethereum/src/mappings/liquity.ts
@@ -3,12 +3,8 @@ import {
   Liquidation as LiquidationEvent,
   LiquityTrove,
 } from '../types/LiquityTrove/LiquityTrove';
-import {
-  StabilityPool,
-} from '../types/StabilityPool/StabilityPool';
-import {
-  LiquityPriceFeed,
-} from '../types/LiquityPriceFeed/LiquityPriceFeed';
+import { ETHGainWithdrawn, StabilityPool } from '../types/StabilityPool/StabilityPool';
+import { LastGoodPriceUpdated, LiquityPriceFeed } from '../types/LiquityPriceFeed/LiquityPriceFeed';
 import { Address } from '@graphprotocol/graph-ts';
 
 export function handleLiquidation(event: LiquidationEvent): void {
@@ -17,7 +13,10 @@ export function handleLiquidation(event: LiquidationEvent): void {
   let pool = StabilityPool.bind(trove.stabilityPool());
   let priceFeed = LiquityPriceFeed.bind(trove.priceFeed());
 
-  const liquidation = new Liquidation(event.address.toHexString());
+  const liquidationId =
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString();
+
+  const liquidation = new Liquidation(liquidationId);
 
   liquidation.timestamp = event.block.timestamp;
   liquidation.txHash = event.transaction.hash;
@@ -25,10 +24,14 @@ export function handleLiquidation(event: LiquidationEvent): void {
   liquidation.liquidatedCollateral = event.params._liquidatedColl;
   liquidation.collGasCompensation = event.params._collGasCompensation;
   liquidation.tokenGasCompensation = event.params._LUSDGasCompensation;
-  liquidation.ethPrice = priceFeed.lastGoodPrice();
   liquidation.strategyBalance = pool.getDepositorETHGain(
     Address.fromString('0xf33fb13b1cBbCC4Ae28026Ec9a433A1AD6fea172'),
   );
+  liquidation.ethPrice = priceFeed.lastGoodPrice();
 
   liquidation.save();
 }
+
+export function handleETHGainWithdraw(_event: ETHGainWithdrawn): void {}
+
+export function handleLastGoodPriceUpdated(_event: LastGoodPriceUpdated): void {}
