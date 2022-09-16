f397e013587a08fc37fe1a75519af7d116630ed5
0xrin1 <0xrin1@protonmail.com>
2022-09-16 11:44:15 +0200

    max price approach

--- subgraph/ethereum/src/mappings/liquity.ts
+++ subgraph/ethereum/src/mappings/liquity.ts
@@ -1,13 +1,18 @@
-import { Liquidation } from '../types/schema';
+import { Liquidation, LiquidationPriceTracker } from '../types/schema';
 import {
   Liquidation as LiquidationEvent,
   LiquityTrove,
 } from '../types/LiquityTrove/LiquityTrove';
-import { ETHGainWithdrawn, StabilityPool } from '../types/StabilityPool/StabilityPool';
-import { LastGoodPriceUpdated, LiquityPriceFeed } from '../types/LiquityPriceFeed/LiquityPriceFeed';
-import { Address } from '@graphprotocol/graph-ts';
+import {
+  ETHGainWithdrawn,
+  StabilityPool,
+} from '../types/StabilityPool/StabilityPool';
+import { LiquityPriceFeed } from '../types/LiquityPriceFeed/LiquityPriceFeed';
+import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts';
 
 export function handleLiquidation(event: LiquidationEvent): void {
+  let priceTracker = getPriceTracker('0');
+
   // bind the contract to the address that emitted the event
   let trove = LiquityTrove.bind(event.address);
   let pool = StabilityPool.bind(trove.stabilityPool());
@@ -25,13 +30,54 @@ export function handleLiquidation(event: LiquidationEvent): void {
   liquidation.collGasCompensation = event.params._collGasCompensation;
   liquidation.tokenGasCompensation = event.params._LUSDGasCompensation;
   liquidation.strategyBalance = pool.getDepositorETHGain(
-    Address.fromString('0x2b1Ce1eF546051d38A8e23917520a7A9C05Da281'),
+    // Address.fromString('0x2b1Ce1eF546051d38A8e23917520a7A9C05Da281'), // local
+    Address.fromString('0x9043268b2e280dE7DF8AAfe7FEb86e553bd90FdD'), // prod
   );
   liquidation.ethPrice = priceFeed.lastGoodPrice();
+  liquidation.highestPrice = priceTracker.highestPrice;
 
   liquidation.save();
 }
 
-export function handleETHGainWithdraw(_event: ETHGainWithdrawn): void {}
+export function handleETHGainWithdraw(_event: ETHGainWithdrawn): void {
+  let priceTracker = getPriceTracker('0');
 
-export function handleLastGoodPriceUpdated(_event: LastGoodPriceUpdated): void {}
+  priceTracker.highestPrice = BigInt.fromString('0');
+
+  priceTracker.save();
+}
+
+export function trackHighestPrice(block: ethereum.Block): void {
+  if (!block.number.mod(BigInt.fromString('50')).equals(BigInt.fromString('0'))) return;
+
+  let priceTracker = getPriceTracker('0');
+
+  if (priceTracker == null) {
+    priceTracker = new LiquidationPriceTracker('0');
+    priceTracker.highestPrice = BigInt.fromString('0');
+  }
+
+  let priceFeed = LiquityPriceFeed.bind(
+    // Address.fromString('0x53CbbE1a2cbC42841bdbF2aC855E245f822c768B'), // local
+    Address.fromString('0x4c517d4e2c851ca76d7ec94b805269df0f2201de'), // prod
+  );
+  const priceResult = priceFeed.try_lastGoodPrice();
+
+  if (!priceResult.reverted && priceTracker.highestPrice.lt(priceResult.value)) {
+    priceTracker.highestPrice = priceResult.value;
+  }
+
+  priceTracker.save();
+}
+
+function getPriceTracker(id: string): LiquidationPriceTracker {
+  let priceTracker = LiquidationPriceTracker.load('0');
+
+  if (priceTracker == null) {
+    priceTracker = new LiquidationPriceTracker('0');
+    priceTracker.highestPrice = BigInt.fromString('0');
+    priceTracker.save();
+  }
+
+  return priceTracker;
+}
