8d33c373176b3ada6bc1e7694215b6ee82de6d27
brkomir <nenad.milenkovic154@outlook.com>
2022-10-20 16:34:57 +0200

    0xMock return true

--- contracts/mock/Mock0x.sol
+++ contracts/mock/Mock0x.sol
@@ -6,15 +6,15 @@ contract Mock0x {
     uint8 public constant ETH = 2;
 
     fallback() external payable {
-        (uint8 from, uint8 to) = abi.decode(msg.data, (uint8, uint8));
-        console.log("asdjflksajflsadfjsdafasd");
+        // (uint8 from, uint8 to) = abi.decode(msg.data, (uint8, uint8));
+        // console.log("asdjflksajflsadfjsdafasd");
+
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, 1)
+            return(ptr, 0x20)
+        }
     }
 
-    // function exchange() external payable {
-    //     console.log("asdjflksajflsadfjsdafasd");
-    // }
-
-    // function call(bytes calldata data) external payable {
-    //     console.log("asdjflksajflsadfjsdafasd");
-    // }
+    receive() external payable {}
 }
