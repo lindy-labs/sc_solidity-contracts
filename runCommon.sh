f0e7df48d52d4037b4781742bc14a1f0b6e7ec19
fyang1024 <fyang1024@gmail.com>
2022-11-04 22:34:42 +1100

    update runCommon.sh to be more general

--- runCommon.sh
+++ runCommon.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-if [ "$#" -eq 0 ]
+if [ "$#" -eq 2 ]
 then 
-  certoraRun certora/harness/VaultHarness.sol \
-    --verify VaultHarness:certora/specs/common.spec \
+  certoraRun $1 \
+    --verify $2:certora/specs/common.spec \
     --optimistic_loop \
     --packages @openzeppelin=node_modules/@openzeppelin \
-    --msg "verifying Vault"
-elif [ "$#" -eq 1 ]
+    --msg "verifying $2 contract"
+elif [ "$#" -eq 3 ]
 then
-  certoraRun certora/harness/VaultHarness.sol \
-    --verify VaultHarness:certora/specs/common.spec \
+  certoraRun $1 \
+    --verify $2:certora/specs/common.spec \
     --optimistic_loop \
     --packages @openzeppelin=node_modules/@openzeppelin \
-    --rule "$1" \
-    --msg "verifying rule $1 for Vault"
+    --rule "$3" \
+    --msg "verifying rule $3 for $2 contract"
 else
-  echo "You can only only one argument to specify which rule to verify"
+  echo "You need to supply 2 or 3 arguments to run the command, e.g., sh runCommon.sh certora/harness/VaultHarness.sol VaultHarness noRevert"
 fi 
